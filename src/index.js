e317a8bf9eeb36e5724cf6329b9ca170b89b08a3
S. O <so_933@hotmail.com>
2018-06-14 08:54:48 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MapWithAMarker from "./components/maps";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(<MapWithAMarker />, document.getElementById("root"));
